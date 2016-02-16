64cf7942f39645c49977d84b6bfc2abf00c23c1b
rjb <rjb-prod@users.noreply.github.com>
2016-02-17 11:51:03 +1300

    Update DBRecord.java

--- src/db/DBRecord.java
+++ src/db/DBRecord.java
@@ -1,6 +1,6 @@
 /*
  * DBRecord.java    version 1.0   date 16/12/2015
- * By Rebecca Blundell 91023656 
+ * By rjb 
  */
 
 
@@ -11,7 +11,7 @@ package db;
 /**
  *This class creates an object from a database record
  * 
- * @author Rebecca Blundell 91023656
+ * @author rjb
  */
 public class DBRecord {
 
