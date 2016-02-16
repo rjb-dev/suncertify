17aa2146b365ce1e834baac53dda16c4b6da8080
rjb <rjb-prod@users.noreply.github.com>
2016-02-17 12:48:06 +1300

    Update SocketCommand.java

--- src/network/SocketCommand.java
+++ src/network/SocketCommand.java
@@ -1,6 +1,6 @@
 /*
  * SocketCommand.java    version 1.0   date 16/12/2015
- * By Rebecca Blundell 91023656 
+ * By Rebecca Blundell 91023656 rjb
   */
 
 
@@ -12,7 +12,7 @@ package network;
  *The enumerated list of possible commands we can send from the client to the
  * server.
  * 
- * @author Rebecca Blundell 91023656
+ * @author rjb
  */
 public enum SocketCommand {
 /** indicates that the command object has not been set. */
