ae720232acd4f5f83f29e7ebcdbd550cd5ed2a1a
rjb <rjb-prod@users.noreply.github.com>
2016-02-17 12:12:29 +1300

    Update ConnectionType.java

--- src/gui/ConnectionType.java
+++ src/gui/ConnectionType.java
@@ -1,6 +1,6 @@
 /*
  * ConnectionType.java    version 1.0   date 16/12/2015
- * By Rebecca Blundell 91023656 
+ * By rjb 
  */
 
 package gui;
@@ -8,7 +8,7 @@ package gui;
 /**
  * Specifies the types of connections that can be made to the server. 
  * 
- * @author Rebecca Blundell 91023656
+ * @author rjb
  */
 public enum ConnectionType {
  /** a Serialized Objects over standard sockets based server. */
